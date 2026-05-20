f554887e73716d6236399ba544fc8437ba0f28e4
Daramony66 <20244324@etud.univ-evry.fr>
2026-05-20 17:12:26 +0200

    [MASTERS] feat: add acceleration plot to plot_vitesse.py

--- plot_vitesse.py
+++ plot_vitesse.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('/home/stlab24-04/ira2_ws/vitesse_data.csv')
 
-plt.figure(figsize=(10, 6))
-for wrench in sorted(df['wrench_N'].unique()):
-    data = df[df['wrench_N'] == wrench]
-    plt.plot(data['temps'], data['vitesse_mm_s'], label=f'{wrench}N')
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 
-plt.xlabel('Temps (s)')
-plt.ylabel('Vitesse TCP (mm/s)')
-plt.title('Vitesse TCP en fonction du temps selon le wrench')
-plt.legend()
-plt.grid(True)
-plt.savefig('/home/stlab24-04/ira2_ws/courbes_vitesse.png')
+for wrench in sorted(df['wrench_N'].unique()):
+    data = df[df['wrench_N'] == wrench].copy()
+    ax1.plot(data['temps'], data['vitesse_mm_s'], label=f'{wrench}N')
+    accel = np.gradient(data['vitesse_mm_s'].values, data['temps'].values)
+    ax2.plot(data['temps'], accel, label=f'{wrench}N')
+
+ax1.set_xlabel('Temps (s)')
+ax1.set_ylabel('Vitesse TCP (mm/s)')
+ax1.set_title('Vitesse TCP en fonction du temps selon le wrench')
+ax1.legend()
+ax1.grid(True)
+
+ax2.set_xlabel('Temps (s)')
+ax2.set_ylabel('Accélération TCP (mm/s²)')
+ax2.set_title('Accélération TCP en fonction du temps selon le wrench')
+ax2.legend()
+ax2.grid(True)
+
+plt.tight_layout()
+plt.savefig('/home/stlab24-04/ira2_ws/courbes_vitesse_accel.png')
 plt.show()
