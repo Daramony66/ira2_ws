9f1f4ed07f4094cd73e18d631be4266944d48ee2
Daramony66 <20244324@etud.univ-evry.fr>
2026-05-27 11:02:56 +0200

    [MASTERS][plot_vitesse] fix: plot by essai instead of wrench

--- plot_vitesse.py
+++ plot_vitesse.py
@@ -6,11 +6,12 @@ df = pd.read_csv('/home/stlab24-04/ira2_ws/vitesse_data.csv')
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 
-for wrench in sorted(df['wrench_N'].unique()):
-    data = df[df['wrench_N'] == wrench].copy()
-    ax1.plot(data['temps'], data['vitesse_mm_s'], label=f'{wrench}N')
+for essai in sorted(df['essai'].unique()):
+    data = df[df['essai'] == essai].copy()
+    wrench = data['wrench_N'].iloc[0]
+    ax1.plot(data['temps'], data['vitesse_mm_s'], label=f'essai {essai} ({wrench}N)')
     accel = np.gradient(data['vitesse_mm_s'].values, data['temps'].values)
-    ax2.plot(data['temps'], accel, label=f'{wrench}N')
+    ax2.plot(data['temps'], accel, label=f'essai {essai} ({wrench}N)')
 
 ax1.set_xlabel('Temps (s)')
 ax1.set_ylabel('Vitesse TCP (mm/s)')
